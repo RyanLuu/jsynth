9ae4b3465baf0558c15aa06f829bb1e7c48ea07b
Ryan Luu <rl65536@gmail.com>
2021-10-21 00:33:15 -0500

    Oscillator.Type -> Shape, asymmetric slew, update .gitignore

--- Oscillator.java
+++ Oscillator.java
@@ -2,36 +2,36 @@ import java.util.function.DoubleUnaryOperator;
 
 public class Oscillator extends Module {
 
-    public enum Type {
+    public enum Shape {
         SINE(Math::sin),
         SQUARE(d -> Math.round(d / (2 * Math.PI)) * 2 - 1),
         SAWTOOTH(d -> (d - Math.PI) / Math.PI),
         TRIANGLE(d -> (d < Math.PI) ? (2 * d / Math.PI - 1) : (-2 * d / Math.PI + 3));
         private DoubleUnaryOperator shape;
-        private Type(DoubleUnaryOperator shape) {
+        private Shape(DoubleUnaryOperator shape) {
             this.shape = shape;
         }
     }
 
-    Type type;
+    Shape shape;
     double angle;
 
-    public Oscillator(Type type, InputNode voltPerOct, InputNode amplitude, InputNode offset) {
+    public Oscillator(Shape shape, InputNode voltPerOct, InputNode amplitude, InputNode offset) {
         super(1, voltPerOct, amplitude, offset);
-        this.type = type;
+        this.shape = shape;
         this.angle = 0;
     }
 
-    public Oscillator(Type type, InputNode voltPerOct) {
-        this(type, voltPerOct, new ConstNode(1), new ConstNode(0));
+    public Oscillator(Shape shape, InputNode voltPerOct) {
+        this(shape, voltPerOct, new ConstNode(1), new ConstNode(0));
     }
 
-    public Oscillator(Type type, double voltPerOct, double amplitude, double offset) {
-        this(type, new ConstNode(voltPerOct), new ConstNode(amplitude), new ConstNode(offset));
+    public Oscillator(Shape shape, double voltPerOct, double amplitude, double offset) {
+        this(shape, new ConstNode(voltPerOct), new ConstNode(amplitude), new ConstNode(offset));
     }
 
-    public Oscillator(Type type, double voltPerOct) {
-        this(type, new ConstNode(voltPerOct), new ConstNode(1), new ConstNode(0));
+    public Oscillator(Shape shape, double voltPerOct) {
+        this(shape, new ConstNode(voltPerOct), new ConstNode(1), new ConstNode(0));
     }
 
     @Override
@@ -39,7 +39,7 @@ public class Oscillator extends Module {
         double frequency = Frequency.toHz(inputs[0]);
         this.angle += (2 * Math.PI * frequency / Time.SAMPLE_RATE);
         this.angle %= 2 * Math.PI;
-        return new double[] { this.type.shape.applyAsDouble(this.angle) * inputs[1] + inputs[2] };
+        return new double[] { this.shape.shape.applyAsDouble(this.angle) * inputs[1] + inputs[2] };
     }
 
 }
--- Slew.java
+++ Slew.java
@@ -1,12 +1,26 @@
 public class Slew extends Module {
 
-    private double rate;
+    public enum Type {
+        RISE, FALL, BOTH
+    }
+
+    private final double rate;
+    private final Type type;
     private double lastOutput;
 
-    public Slew(InputNode input, double rate, double initialOutput) {
+    public Slew(InputNode input, Type type, double rate, double initialOutput) {
         super(1, input);
         this.rate = rate / Time.SAMPLE_RATE;
         this.lastOutput = initialOutput;
+        this.type = type;
+    }
+
+    public Slew(InputNode input, Type type, double rate) {
+        this(input, type, rate, 0);
+    }
+
+    public Slew(InputNode input, double rate, double initialOutput) {
+        this(input, Type.BOTH, rate, initialOutput);
     }
 
     public Slew(InputNode input, double rate) {
@@ -15,9 +29,9 @@ public class Slew extends Module {
 
     @Override
     public double[] compute(double[] inputs) {
-        if (inputs[0] > lastOutput + rate) {
+        if (type != Type.FALL && inputs[0] > lastOutput + rate) {
             lastOutput = lastOutput + rate;
-        } else if (inputs[0] < lastOutput - rate) {
+        } else if (type != Type.RISE && inputs[0] < lastOutput - rate) {
             lastOutput = lastOutput - rate;
         } else {
             lastOutput = inputs[0];
